fd61840912fb4128785405144374a343099edcec
Muhammad Farhan Gultom <pitjarusfarhan@outlook.com>
2025-08-20 21:07:15 +0700

    fixing sms gateway again

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -90,7 +90,7 @@ class UserController extends Controller
                 // Check the response
                 if ($response->successful()) {
                     // Success logic
-                    return new UserResource(true, "Token sent successfully {$response.json()}", array());
+                    return new UserResource(true, "Token sent successfully", array());
                 } else {
                     // Error handling
                     return new UserResource(false, 'Failed to send Token', array());
