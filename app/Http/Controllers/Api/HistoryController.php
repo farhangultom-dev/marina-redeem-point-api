f876f8aff56af049b1fbaed9b2c8c9ac09de8c9a
Muhammad Farhan Gultom <pitjarusfarhan@outlook.com>
2025-10-23 10:20:07 +0700

    fixing get history

--- app/Http/Controllers/Api/HistoryController.php
+++ app/Http/Controllers/Api/HistoryController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\HistoryRedeem;
-use App\Models\Voucher;
 use App\Http\Controllers\Controller;
+use App\Models\Voucher;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Carbon\Carbon;
 
 class HistoryController extends Controller
 {
@@ -16,15 +15,15 @@ class HistoryController extends Controller
         $baseUrl = url('storage/image_vouchers/');
 
         $datas = DB::table('history_redeems')
-        ->join('vouchers', 'history_redeems.voucher_id', '=', 'vouchers.id')
-        ->join('partners', 'vouchers.id_partner', '=', 'partners.id')
-        ->where('history_redeems.user_id', $request->query('user_id'))
-        ->select('history_redeems.user_id',
-        'history_redeems.voucher_id',
-        'vouchers.point_needed as point', 
-        DB::raw("DATE(history_redeems.created_at) as tanggal_penukaran"),
-        DB::raw("CONCAT('$baseUrl/', image1) as image1"), 'partners.city')
-        ->get();
+            ->join('vouchers', 'history_redeems.voucher_id', '=', 'vouchers.id')
+            ->join('partners', 'vouchers.id_partner', '=', 'partners.user_id')
+            ->where('history_redeems.user_id', $request->query('user_id'))
+            ->select('history_redeems.user_id',
+                'history_redeems.voucher_id',
+                'vouchers.point_needed as point',
+                DB::raw('DATE(history_redeems.created_at) as tanggal_penukaran'),
+                DB::raw("CONCAT('$baseUrl/', image1) as image1"), 'partners.city')
+            ->get();
 
         return response()->json([
             'status' => 'true',
@@ -37,16 +36,16 @@ class HistoryController extends Controller
         $baseUrl = url('storage/image_vouchers/');
 
         $datas = DB::table('history_redeems')
-        ->join('vouchers', 'history_redeems.voucher_id', '=', 'vouchers.id')
-        ->join('partners', 'vouchers.id_partner', '=', 'partners.id')
-        ->join('users', 'history_redeems.user_id', "=", 'users.id')
-        ->where('partners.id', $request->query('partner_id'))
-        ->select('partners.id as partner_id',
-        'history_redeems.voucher_id','users.full_name as customer_name','users.phone_number',
-        'vouchers.point_needed as point', 
-        DB::raw("DATE(history_redeems.created_at) as tanggal_penukaran"),
-        DB::raw("CONCAT('$baseUrl/', image1) as image1"), 'partners.city')
-        ->get();
+            ->join('vouchers', 'history_redeems.voucher_id', '=', 'vouchers.id')
+            ->join('partners', 'vouchers.id_partner', '=', 'partners.id')
+            ->join('users', 'history_redeems.user_id', '=', 'users.id')
+            ->where('partners.id', $request->query('partner_id'))
+            ->select('partners.id as partner_id',
+                'history_redeems.voucher_id', 'users.full_name as customer_name', 'users.phone_number',
+                'vouchers.point_needed as point',
+                DB::raw('DATE(history_redeems.created_at) as tanggal_penukaran'),
+                DB::raw("CONCAT('$baseUrl/', image1) as image1"), 'partners.city')
+            ->get();
 
         return response()->json([
             'status' => 'true',
@@ -56,10 +55,11 @@ class HistoryController extends Controller
 
     public function getNotification()
     {
-        $data = Voucher::select('nama_merchant','description',DB::raw("DATE(created_at) as created_date"))
-        ->where('created_at', '>=', Carbon::now()->subDays(7))
-        ->where('is_approved', '1')
-        ->get();
+        $data = Voucher::select('nama_merchant', 'description', DB::raw('DATE(created_at) as created_date'))
+            ->where('created_at', '>=', Carbon::now()->subDays(7))
+            ->where('is_approved', '1')
+            ->get();
+
         return response()->json([
             'status' => 'true',
             'data' => $data,
